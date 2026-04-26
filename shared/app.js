333313dc2f64ef4988fe45206369ef46e3591410
Azizul Karim <karimazizul1@gmail.com>
2026-04-26 09:55:27 +0600

    wly-155: some renaming and impl of marker gap

--- shared/app.js
+++ shared/app.js
@@ -1,20 +1,24 @@
 // List operations
 document.querySelectorAll("ol.list").forEach((list) => {
-  const prefix = list.dataset.listPrefix;
-  const suffix = list.dataset.listSuffix;
+  const markerGap = list.dataset.listMarkerGap;
+  const markerPrefix = list.dataset.listMarkerPrefix;
+  const markerSuffix = list.dataset.listMarkerSuffix;
   const indentLeft = list.dataset.listIndentLeft;
   const indentRight = list.dataset.listIndentRight;
   const gapY = list.dataset.listGapY;
 
-  // Set prefix and suffix
-  if (prefix) {
-    list.style.setProperty("--list-marker-prefix", `"${prefix}"`);
+  // Set marker props
+  if (markerPrefix) {
+    list.style.setProperty("--list-marker-prefix", `"${markerPrefix}"`);
   } else {
     // check if suffix exist then set prefix to empty string to override default `(` prefix
     // this handle list markers such as `1.`, `i.` etc where only suffix exist and there shouldn't be any prefix
-    if (suffix) list.style.setProperty("--list-marker-prefix", `""`);
+    if (markerSuffix) list.style.setProperty("--list-marker-prefix", `""`);
   }
-  if (suffix) list.style.setProperty("--list-marker-suffix", `"${suffix}"`);
+  if (markerSuffix)
+    list.style.setProperty("--list-marker-suffix", `"${markerSuffix}"`);
+
+  if (markerGap) list.style.setProperty("--list-marker-gap", `${markerGap}`);
 
   // Set rest of the properties
   if (indentLeft) list.style.setProperty("--list-indent-left", `${indentLeft}`);
